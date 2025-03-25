91000a27cc39e064495475a62269bf49fd7422b7
Fares Frini <frini.mohamedfares@esprit.tn>
2025-03-25 21:30:36 +0100

    new project + footer

---
+++ src/components/layouts/footer.tsx
@@ -0,0 +1,62 @@
+import { navItems } from "@/constants/navItems";
+import Link from "next/link";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa6";
+
+export default function Footer() {
+    return (
+        <footer className="container mx-auto p-4 md:px-6 md:py-8">
+            <div className="flex flex-col items-center gap-4 sm:justify-between lg:flex-row">
+                <div className="hidden flex-wrap items-center gap-2 text-sm lg:flex">
+                    <Link
+                        href={"#"}
+                        target="_blank"
+                        className="font-semibold"
+                    >
+                        © 2025 All rights reserved.
+                    </Link>
+                </div>
+
+                <div className="hidden items-center gap-4 md:flex ml-auto">
+                    <ul className="ffont-['space_mono'] flex flex-wrap items-center text-sm text-gray-500 dark:text-gray-400">
+                        {navItems.map((item, index) => (
+                            <Link
+                                key={index}
+                                href={item.link}
+                                className="hover:focus:block p-4 px-4 text-gray-600 transition-all duration-75 hover:underline focus:font-bold dark:text-gray-400 dark:hover:text-gray-200"
+                            >
+                                {item.name}
+                            </Link>
+                        ))}
+                    </ul>
+                </div>
+                <a
+                    href="https://www.facebook.com/ieee.tsyp"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-gray-600 dark:text-gray-400"
+                    aria-label="facebook page"
+                >
+                    <FaFacebook />
+                </a>
+                <a
+                    href="https://www.instagram.com/ieee_tsyp/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-gray-600 dark:text-gray-400"
+                    aria-label="instagram page"
+                >
+                    <FaInstagram />
+                </a>
+                <a
+                    href="https://www.linkedin.com/company/ieee-tsyp/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-gray-600 dark:text-gray-400"
+                    aria-label="linkedin page"
+                >
+                    <FaLinkedin />
+                </a>
+            </div>
+        </footer>
+    );
+}
---
+++ src/constants/navItems.tsx
@@ -0,0 +1,30 @@
+export const navItems = [
+    {
+        name: 'Home',
+        link: '/'
+    },
+    {
+        name: 'Tunisia',
+        link: '/tunisia'
+    },
+    {
+        name: 'Speakers',
+        link: '/speakers'
+    },
+    {
+        name: 'About us',
+        link: '/about-us'
+    },
+    {
+        name: 'Agenda',
+        link: '/agenda'
+    },
+    {
+        name: 'Partners',
+        link: '/partners'
+    },
+    {
+        name: 'Contact us',
+        link: '/contact-us'
+    }
+];
